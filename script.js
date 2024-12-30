f593a9fc108d391e760f1967881c86611d0934b6
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-30 21:59:10 +0100

    fixed answers

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ const questions = [
 
 // take the elements from DOM
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -301,19 +301,26 @@ function startQuiz() {
 function showQuestion() {
 
     resetState();
-    
+
     // display question
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     // display answers
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
     })
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
 }
 
 startQuiz();
